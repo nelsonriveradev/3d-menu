69555966743726a2d78d968fd9788a6d185efeb8
nelsonriveradev <nelsonrivera.dev@gmail.com>
2025-04-30 15:57:57 -0400

    Deleting server side code for uploading data. Preparing everything to refactor to mongoDB

--- app/api/upload-ar-model/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { createClient } from "@supabase/supabase-js";
-import { v4 as uuidv4 } from "uuid";
-
-export async function POST(req: Request) {
-  const formData = await req.formData();
-  const fileUSDZ = formData.get("objectUSDZ") as File;
-}
